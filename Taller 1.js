3613d4866db4ef3c0785600546eb9a9472330011
Aidretr0 <146611786+Aidretr0@users.noreply.github.com>
2024-04-09 21:45:21 -0500

    Add files via upload

---
+++ Taller 1.js
@@ -0,0 +1,71 @@
+// 1. Declara una variable llamada nombre y asígnale tu nombre como valor.
+let nombre = "Moises";
+
+// 2. Crea una variable llamada edad y asígnale tu edad.
+let edad = 19; 
+
+// 3. Declara una variable ‘esEstudiante’ y asígnale un valor booleano.
+let esEstudiante = true; 
+
+// 4. Crea una variable llamada puntaje y no le asignes ningún valor inicial.
+let puntaje;
+
+// 5. Declara una variable hobbies que contenga un array con tus hobbies.
+let hobbies = ["Jugar", "Dormir", "Leer"]; 
+
+// 6. Crea un objeto persona que contenga tres propiedades: nombre, edad y esEstudiante.
+let persona = {
+    nombre: nombre,
+    edad: edad,
+    esEstudiante: esEstudiante
+};
+
+// 7. Declara una variable nullVar y asígnale el valor null.
+let nullVar = null;
+
+// 8. Crea una variable indefinida sin asignarle un valor.
+let indefinida;
+
+// 9. Declara una variable numeroHex que contenga un número hexadecimal.
+let numeroHex = 0xFF; 
+
+// 10. Crea una variable nombreCompleto que concatene tu nombre y tu apellido.
+let apellido = "Hurtado"; 
+let nombreCompleto = nombre + " " + apellido;
+
+// 11. Suma dos números y almacena el resultado en una variable suma.
+let num1 = 15;
+let num2 = 10;
+let suma = num1 + num2;
+
+// 12. Resta dos números y guarda el resultado en una variable resta.
+let resta = num1 - num2;
+
+// 13. Multiplica dos números y asigna el resultado a una variable multiplicacion.
+let multiplicacion = num1 * num2;
+
+// 14. Divide dos números y almacena el resultado en una variable division.
+let division = num1 / num2;
+
+// 15. Usa el operador de módulo para encontrar el residuo de 10 dividido por 3 y almacena el resultado en una variable modulo.
+let modulo = 10 % 3;
+
+// 16. Incrementa el valor de una variable contador en 1 usando el operador de incremento.
+let contador = 0;
+contador++;
+
+// 17. Decrementa el valor de una variable contadorDec en 1 usando el operador de decremento.
+let contadorDec = 9;
+contadorDec--;
+
+// 18. Compara dos números para ver si son iguales y almacena el resultado (booleano) en una variable sonIguales.
+let num3 = 8;
+let num4 = 8;
+let sonIguales = num3 === num4;
+
+// 19. Comprueba si un número es mayor que otro y guarda el resultado (booleano) en una variable esMayor.
+let esMayor = num1 > num2;
+
+// 20. Utiliza el operador de asignación compuesta para sumar y asignar un valor a una variable.
+let valor = 5;
+valor += 10; 
